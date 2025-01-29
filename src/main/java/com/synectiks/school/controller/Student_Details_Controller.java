bf7a1a734ab5691d017ae69142c2fba1cbf79cd8
SharathP1 <sharath.madhi@synectiks.com>
2025-01-29 12:51:07 +0530

    added fee details apis

--- src/main/java/com/synectiks/school/controller/Student_Details_Controller.java
+++ src/main/java/com/synectiks/school/controller/Student_Details_Controller.java
@@ -6,11 +6,15 @@ import com.synectiks.school.service.TransportServiceDetails;
 
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
@@ -20,11 +24,11 @@ public class Student_Details_Controller {
 	
 	
 	
+	//POST Methods
 @PostMapping("transport_details")
 public List<Map<String, Object>> postMethodName(@RequestBody List<Map<String, Object>> bustransport) {
     //TODO: process POST request
 	transportServiceDetails.addingTransportDetails(bustransport);
-
     return bustransport;
 }
 
@@ -40,6 +44,31 @@ public String addingParentDetails(@RequestBody Map<String, Object> PersonalDetai
 	return null;
 }
 
+@PostMapping("Fee_Details/{sid}")
+public String addingFeeDetails(@RequestBody Map<String, Object> FeeDetails,@PathVariable String sid) {
+	transportServiceDetails.addingFee(FeeDetails,sid);
+	return null;
+}
 
+
+     //Get Methods
+
+@GetMapping("get_Student_Details")
+public List<Map<String, Object>> getMethodName() throws InterruptedException, ExecutionException {
+	List<Map<String, Object>> studentdetails= transportServiceDetails.getstudentdetails();
+    return studentdetails ;
+}
+
+@GetMapping("get_Personal_Details")
+public List<Map<String, Object>> getMethodName1() throws InterruptedException, ExecutionException {
+	List<Map<String, Object>> personaldetails= transportServiceDetails.getpersonaldetails();
+    return personaldetails ;
+}
+
+@GetMapping("get_Fee_Details")
+public List<Map<String, Object>> getMethodName2() throws InterruptedException, ExecutionException {
+	List<Map<String, Object>> Feedetails= transportServiceDetails.getfeedetails();
+    return Feedetails ;
+}
 
 }
